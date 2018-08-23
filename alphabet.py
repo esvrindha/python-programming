22fcbb3a5ed0762e8c99e35917648d761da435d4
esvrindha <42571473+esvrindha@users.noreply.github.com>
2018-08-23 15:32:53 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,6 +1,6 @@
-let=["a","b","c","d","e","f","g","h","j","i","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+alp=["a","b","c","d","e","f","g","h","j","i","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 n1=raw_input("")
-if (n1 in let):
+if (n1 in alp):
     print("alphabet")
 else:
-    print("no")     
+    print("no")   
