aa3119d6d13e47addffeef23b59eab3b0ae335ac
esvrindha <42571473+esvrindha@users.noreply.github.com>
2018-08-29 15:07:53 +0530

    Update oddinbtn.py

--- oddinbtn.py
+++ oddinbtn.py
@@ -1,5 +1,5 @@
-bindu=int(input(""))
-sathyan=int(input(""))
-for i in range(bindu,sathyan+1):
+bindu=int(input(" "))
+sathyan=int(input(" "))
+for i in range(bindu+1,sathyan+1):
     if(i%2!=0):
         print(i)
