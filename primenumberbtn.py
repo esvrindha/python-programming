06205ba0b7428ce170c547b3b391ca69f2513ea1
esvrindha <42571473+esvrindha@users.noreply.github.com>
2018-09-02 12:42:50 +0530

    Update primenumberbtn.py

--- primenumberbtn.py
+++ primenumberbtn.py
@@ -1,7 +1,7 @@
-vrin=1
-vamp=10
+vrin=int(input(""))
+vamp=int(input(""))
 for a in range(2,vamp+1):
-    k = 0
+    k = 0=
     for i in range(2,a//2+1):
         if(a%i == 0):
             k = k+1
