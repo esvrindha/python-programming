4f31182b9d9d2ce24556daf16c0ded83bbe1c621
esvrindha <42571473+esvrindha@users.noreply.github.com>
2018-11-26 12:14:08 +0530

    Create median.py

---
+++ median.py
@@ -0,0 +1,12 @@
+vrin=input("")
+sdd=[]
+for i in range(0,vrin):
+    vish=int(input())
+    sdd.append(vish)
+sdd.sort()
+if(vrin%2 !=0):
+    print(sdd[(vrin/2)])
+else:
+    bind=vrin/2
+    c=(int(sdd[bind]}+int(sdd[sailu.1]))/2
+    print(c)
