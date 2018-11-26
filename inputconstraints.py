df22cf94346cc3944d4cae4d4b2e614697c7887d
esvrindha <42571473+esvrindha@users.noreply.github.com>
2018-11-26 13:26:29 +0530

    Create inputconstraints.py

---
+++ inputconstraints.py
@@ -0,0 +1,15 @@
+num = int(input(" "))
+storage = []
+result = []
+
+
+for i in range(1,num+1):
+    a = int(input(str(i)))
+    storage.append(a)
+    
+    
+for m in range(len(storage)):
+    b = min(storage)
+    storage.remove(b)
+j = ' ',join(str(i) for i in result)
+print(j)
