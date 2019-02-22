1f9ef52e59ec8bfc892ce94681bb2f769effa956
esvrindha <42571473+esvrindha@users.noreply.github.com>
2019-02-22 12:59:23 +0530

    Create 10no.py

---
+++ 10no.py
@@ -0,0 +1,22 @@
+i=int(raw_input(""))
+if(i==1):
+  print("one")
+if(i==2):
+  print("two")
+if(i==3):
+  print("three")
+if(i==4):
+  print("four")
+if(i==5):
+  print("five")
+if(i==6):
+  print("six")
+if(i==7):
+  print("seven")
+if(i==8):
+  print("eight")
+if(i==9):
+  print("nine")
+if(i==10):
+  print("ten")
+
